016439f3695d61d05296819132199208aa1af6ef
7Bssk7 <arhip.fin3@gmail.com>
2025-09-21 16:51:21 -0700

    Update

--- 210-lab-13.cpp
+++ 210-lab-13.cpp
@@ -34,6 +34,20 @@ void outputVector(vector<int> list){
     vector<int>::iterator found = find(list.begin(), list.end(), MISSED_EXAM);
 
     cout << "CLASS 1241B" << "\n" << endl;
+
     cout << "Number of students in the class: " << list.size() << endl;
+
+    cout << "List of all grades for the exam 1: " << endl;
+    for(int gr: list) cout << gr << " "; cout << endl;
+
+    cout << "Grade of the very first student in the list" << list.front() << endl;
+    cout << "Grafe of the very last student in the list" << list.back() << endl;
+
+    cout << "Did the entire class miss the exam?" << endl;
+    cout << (list.empty() == 0? "No" : "Yes") << endl;
+
+    cout << "Did any of the students miss the exam?(someone recoeved grade of 0)" << endl;
+
+
 
 }
