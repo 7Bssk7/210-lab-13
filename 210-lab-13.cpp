81c26216f57fa3b52efddf518150d3ffaf0cbc7b
7Bssk7 <arhip.fin3@gmail.com>
2025-09-21 16:42:24 -0700

    Update

--- 210-lab-13.cpp
+++ 210-lab-13.cpp
@@ -5,7 +5,29 @@
 #include <numeric>
 using namespace std;
 
+void outputVector(vector<int>);
+
 int main(){
+    ifstream inputFile("grades.txt");
+    vector<int> grades;
+    int temp, i = 0; 
+
+    if(inputFile.is_open()){
+        while(inputFile >> temp){
+            grades.at(i) = temp;
+            ++i;
+        }
+    }
+    else{
+        cout << "Error, file is not opening" << endl;
+        
+        return 1;
+    }
+    outputVector(grades);
 
     return 0;
+}
+
+void outputVector(vector<int> list){
+    
 }
